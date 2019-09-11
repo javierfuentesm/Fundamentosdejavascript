bd0b29403b8da898994b698e8c3e27596a004add
rjfm <javier.fuentesm@hotmail.com>
2019-09-10 21:31:32 -0500

    funciones

---
+++ clase4.js
@@ -0,0 +1,14 @@
+var nombre='Javier', edad = 25
+
+function imprmirEdad(){
+    console.log(`${nombre} tiene ${edad} años` )
+}
+
+imprmirEdad()
+
+
+
+function imprmirEdad2(n,e){
+    console.log(`${n} tiene ${e} años` )
+}
+imprmirEdad2('Yo merengues','23')
