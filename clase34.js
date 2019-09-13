516194bc502a01437dd0c5ad4799a696123ef008
rjfm <javier.fuentesm@hotmail.com>
2019-09-13 14:54:15 -0500

    Promesas en paralelo

---
+++ clase34.js
@@ -0,0 +1,78 @@
+const API_URL = "https://swapi.co/api/";
+
+const PEOPLE_URL = "people/:id";
+
+const lukeurl = `${API_URL}${PEOPLE_URL.replace(":id", 1)}`;
+const options = { crossDomain: true };
+
+function obtenerpersonaje(id) {
+  return new Promise((resolve, reject) => {
+    const url = `${API_URL}${PEOPLE_URL.replace(":id", id)}`;
+
+    $.get(url, options, function(data) {
+      resolve(data);
+    }).fail(() => reject(id));
+  });
+}
+
+function onerror(id) {
+  console.log(`Sucedio un error al cargar el personaje on id = ${id}`);
+}
+var ids=[1,2,3,4,5,6,7]
+/* var promesas = ids.map(function (id) {
+    return obtenerpersonaje(id)
+    
+})
+ */
+
+var promesas = ids.map( (id)=> obtenerpersonaje(id))
+
+Promise.all(promesas).
+then(personajes=>console.log(personajes)).catch(onerror)
+
+
+
+/* obtenerpersonaje(1)
+  .then(personaje=> {
+    console.log(`Hola yo soy ${personaje.name}`);
+    return obtenerpersonaje(2)
+
+  })
+  .then(personaje=> {
+    console.log(`Hola yo soy ${personaje.name}`);
+    return obtenerpersonaje(3)
+
+  })
+  .then(personaje=> {
+    console.log(`Hola yo soy ${personaje.name}`);
+    return obtenerpersonaje(4)
+
+  })
+  .then(personaje=> {
+    console.log(`Hola yo soy ${personaje.name}`);
+    return obtenerpersonaje(5)
+
+  })
+  .then(personaje=> {
+    console.log(`Hola yo soy ${personaje.name}`);
+    return obtenerpersonaje(6)
+
+  })
+  .then(personaje=> {
+    console.log(`Hola yo soy ${personaje.name}`);
+    return obtenerpersonaje(7)
+
+  })
+  .then(personaje=> {
+    console.log(`Hola yo soy ${personaje.name}`);
+    return obtenerpersonaje(8)
+
+  })
+  .then(personaje=> {
+    console.log(`Hola yo soy ${personaje.name}`);
+    return obtenerpersonaje(9)
+
+  })
+
+  .catch(onerror);
+ */
